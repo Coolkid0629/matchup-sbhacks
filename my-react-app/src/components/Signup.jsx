02a576bd1a0296ef542dbd2b10f1eb2ce808c2d6
Aras Janusonis <aras@janusonis.net>
2025-01-11 14:14:48 -0800

    signup update

--- my-react-app/src/components/Signup.jsx
+++ my-react-app/src/components/Signup.jsx
@@ -1,15 +1,14 @@
+import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import './Signup.css';
+import './Login/Login.css';
 
-function BasicExample() {
+function Signup() {
   return (
-    <Container className="mt-5 d-flex flex-column align-items-center signup-container">
-      <h1 className="mb-5 signup-title">
-        SIGNUP
-      </h1>
-      <Form className="signup-form">
+    <div className="login-container">
+      <Form className="login-form">
+        <h2 className="login-title">SIGNUP</h2>
+        
         <Form.Group className="mb-4" controlId="formBasicEmail">
           <Form.Control 
             type="email" 
@@ -26,15 +25,32 @@ function BasicExample() {
           />
         </Form.Group>
 
+        <Form.Group className="mb-4" controlId="formBasicConfirmPassword">
+          <Form.Control 
+            type="password" 
+            placeholder="Confirm Password"
+            className="form-input"
+          />
+        </Form.Group>
+
         <Button 
           variant="primary" 
           type="submit" 
-          className="w-100 submit-button mt-4">
+          className="submit-button w-100">
           Sign Up
         </Button>
+
+        <div className="text-center mt-3">
+          <p>
+            Already have an account?{" "}
+            <a href="#login" className="sign-up">
+              Login
+            </a>
+          </p>
+        </div>
       </Form>
-    </Container>
+    </div>
   );
 }
 
-export default BasicExample;
+export default Signup;
