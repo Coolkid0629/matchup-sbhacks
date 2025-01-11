8e03306ec6efa737ebdd748655a2889a4ec855e1
alokthakrar <alokthakrar1@gmail.com>
2025-01-11 14:58:02 -0800

    added functioning navbar

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login.jsx'
 import Signup from './components/Signup.jsx'
-import NavbarComponent from './components/NavBarComponent.jsx';
+import NavbarComponent from './components/Navbar/NavbarComponent.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './components/Login/Login.jsx'
-import Signup from './components/Signup.jsx'
-import Navbar from './components/Navbar/NavbarComponent.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
