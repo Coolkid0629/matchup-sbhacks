8637e00157548619c9c0a1602a1bd3055f29d210
alokthakrar <alokthakrar1@gmail.com>
2025-01-11 12:49:52 -0800

    test

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import NavBar from './components/NavBar.jsx'
+import Login from './components/Login.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <NavBar /> 
+    <App />
+    <Login />
+  </StrictMode>,
+)
