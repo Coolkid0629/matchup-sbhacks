ff2b0c8fd65bde0a8467cd5ef0af44c103122697
Kaustubh Pawar <kaustubh0629@gmail.com>
2025-01-11 14:49:08 -0800

    navbar and some login

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './components/NavbarComponent'
+import NavbarComponent from './components/Navbar/NavbarComponent.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
