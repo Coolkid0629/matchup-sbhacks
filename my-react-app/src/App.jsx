4ede002a71e4d532b4a142ebb7e79296bfcb1513
Kaustubh Pawar <kaustubh0629@gmail.com>
2025-01-11 15:58:52 -0800

    new 4

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,17 +4,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login.jsx'
 import Signup from './components/Signup.jsx'
 import NavbarComponent from './components/Navbar/NavbarComponent.jsx';
-import Landing from './components/Landing/Landing.jsx';
+import FooterComponent from './components/Footer/FooterComponent.jsx';
+import Landing from './components/Landing/landing.jsx';
 
 function App() {
   return (
     <Router>
-      <NavbarComponent/>
-      <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-      </Routes>
+      <div className="app-container">
+        <NavbarComponent/>
+        <main className="main-content">
+          <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        <FooterComponent/>
+      </div>
     </Router>
   )
 }
