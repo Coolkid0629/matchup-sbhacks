c9111247f1a74a9f2a786ffd341dde63881d274d
alokthakrar <alokthakrar1@gmail.com>
2025-01-11 13:55:44 -0800

    add the navbar

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import NavbarComponent from './components/NavbarComponent.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <NavbarComponent/> 
     <App />
   </StrictMode>,
 )
